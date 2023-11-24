2c258f239898a80eaa46c0abdaa7c3b6467058ce
ben <benrobertson9876@gmail.com>
2023-11-24 16:07:58 +1100

    Update noop for set file path

--- tasks/set_long_paths_windows.ps1
+++ tasks/set_long_paths_windows.ps1
@@ -1,5 +1,11 @@
 # enable long path support Windows 2016 or later.
-if ( $env:PT__noop == 'true' ) {
+if ( $env:PT__noop == "true" ) {
+    echo "Running in noop: bailing out"
+    exit 0
+}
+if ( $env:PT_noop -eq "true" )
+{
+    echo "Running in noop: bailing out"
     exit 0
 }
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
